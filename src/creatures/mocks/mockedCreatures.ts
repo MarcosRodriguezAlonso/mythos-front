65640826125248fdb50b7eb68103af5b6da49623
Marcos <e.marcosra@gmail.com>
2024-06-05 13:50:08 +0200

    Create mocked creatures

---
+++ src/creatures/mocks/mockedCreatures.ts
@@ -0,0 +1,25 @@
+import type Creature from "../../types";
+
+export const mockKtulu: Creature = {
+  _id: "1",
+  name: "Ktulu",
+  lineage: "",
+  imageUrl: "",
+  description: "",
+  author: "Howie",
+  augury: true,
+  alternativeText: "",
+};
+
+export const mockDigon: Creature = {
+  _id: "2",
+  name: "Digon",
+  lineage: "",
+  imageUrl: "",
+  description: "",
+  author: "Howie",
+  augury: false,
+  alternativeText: "",
+};
+
+export const mockCreatures: Creature[] = [mockKtulu, mockDigon];
