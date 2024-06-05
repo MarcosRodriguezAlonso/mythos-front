5e94c192cce8985a7844c83d045b91b0c1b85f6f
Marcos <e.marcosra@gmail.com>
2024-06-05 12:43:50 +0200

    Create CreatureClient

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode></React.StrictMode>
